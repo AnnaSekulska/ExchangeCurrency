19a7e0bc8c6d7c9032587c94a4c90843337609f3
Ania <annasekulska3@gmail.com>
2019-05-24 12:00:23 +0900

    - refactor tests in ActiveCurrenciesServiceImplTest - create new test

--- src/test/java/com/sekulska/services/impl/ActiveCurrenciesServiceImplTest.java
+++ src/test/java/com/sekulska/services/impl/ActiveCurrenciesServiceImplTest.java
@@ -2,6 +2,7 @@ package com.sekulska.services.impl;
 
 import com.sekulska.datacheck.impl.ActiveCurrenciesChecker;
 import com.sekulska.model.ActiveCurrencyInfo;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -24,15 +25,23 @@ public class ActiveCurrenciesServiceImplTest {
     @InjectMocks
     ActiveCurrenciesServiceImpl cut;
 
-    @Test
-    public void testCheckIfMethodReturnedCorrectlyListOfActiveCurrencyInfo() throws IOException {
-        String input = "{\"AED\" : \"United Arab Emirates Dirham\",\n" +
-                "    \"AFN\" : \"Afghan Afghani\"}";
+    @Before
+    public void loadActiveCurrenciesInfos() throws IOException {
+        String input = "{\"AFN\" : \"Afghan Afghani\",\n" +
+                " \"WZX\" : \"WZ WZZ WZZX\",\n"
+               + "    \"AED\" : \"United Arab Emirates Dirham\"}";
 
         Mockito.when(activeCurrenciesChecker.getActiveCurrencies()).thenReturn(input);
+        cut.loadActiveCurrencies();
+    }
+
+    @Test
+    public void testCheckIfMethodReturnedCorrectlyListOfActiveCurrencyInfo() throws IOException {
+
         List<ActiveCurrencyInfo> activeCurrencyInfos = cut.getActiveCurrencies();
 
         List<ActiveCurrencyInfo> expected = new ArrayList<>();
+        expected.add(new ActiveCurrencyInfo("WZX", "WZ WZZ WZZX"));
         expected.add(new ActiveCurrencyInfo("AED", "United Arab Emirates Dirham"));
         expected.add(new ActiveCurrencyInfo("AFN", "Afghan Afghani"));
 
@@ -42,13 +51,23 @@ public class ActiveCurrenciesServiceImplTest {
 
     @Test
     public void testCheckIfReturnedListOfActiveCurrencyInfoHasCorrectlySize() throws IOException {
-        String input = "{\"AED\" : \"United Arab Emirates Dirham\",\n" +
-                "    \"AFN\" : \"Afghan Afghani\"}";
-
-        Mockito.when(activeCurrenciesChecker.getActiveCurrencies()).thenReturn(input);
-
-        assertEquals(2, cut.getActiveCurrencies().size());
-
+        assertEquals(3, cut.getActiveCurrencies().size());
     }
+
+    @Test
+    public void testCheckIfReturnedListIsSortedAlphabetically(){
+        List<ActiveCurrencyInfo> activeCurrencyInfos = cut.getActiveCurrencies();
+
+       assertEquals("WZX", cut.getActiveCurrencies().get(2).getShortName());
+       assertEquals("WZ WZZ WZZX", cut.getActiveCurrencies().get(2).getFullName());
+
+       assertEquals("AFN", cut.getActiveCurrencies().get(1).getShortName());
+       assertEquals("Afghan Afghani", cut.getActiveCurrencies().get(1).getFullName());
+
+       assertEquals("AED", cut.getActiveCurrencies().get(0).getShortName());
+       assertEquals("United Arab Emirates Dirham", cut.getActiveCurrencies().get(0).getFullName());
+    }
+
+
 
 }
